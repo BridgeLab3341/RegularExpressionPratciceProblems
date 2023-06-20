1fd2fa245e049ddde4e41a3f82f053696fa0bbc4
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-20 14:20:59 +0530

    [somu]Add:Match the string that has a followed by two to three b.

---
+++ RegularExpressionPracticeProblems/PracticeProblems.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpressionPracticeProblems
+{
+    internal class PracticeProblems
+    {
+        public void MatchString()
+        {
+            Console.WriteLine("Write a String That contains 'a' and 'b' ");
+            string input =Convert.ToString(Console.ReadLine());
+            string pattern = "^[a]{1}[b]{2,}";
+            Regex regex= new Regex(pattern);
+            if(regex.IsMatch(input))
+            {
+                Console.WriteLine("The given string {0} matches With Pattern",input);
+            }
+            else
+            {
+                Console.WriteLine("The Given string {0} Not Matches With Pattern",input);
+            }
+        }
+    }
+}
---
+++ RegularExpressionPracticeProblems/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace RegularExpressionPracticeProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose Option to perform the operation\n1.matches a string that has an ‘a’ followed by two to three 'b'.\n2.Exit");
+                int option=Convert.ToInt32(Console.ReadLine());
+                PracticeProblems practice=new PracticeProblems();
+               switch (option)
+                {
+                    case 1:
+                        practice.MatchString();
+                        break;
+                    case 2:
+                        flag= false;
+                        break;
+                }
+            }
+        }
+    }
+}
